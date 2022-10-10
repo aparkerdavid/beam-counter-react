4321d9a8bb81c40aee6bafae994a6d7c70a39636
Parker Rueve <parkerrueve@gmail.com>
2022-10-09 21:14:48 -0400

    less gross

--- src/live-state.ts
+++ src/live-state.ts
@@ -5,26 +5,15 @@ let stateProxy;
 let channel;
 
 export function useLiveState(socket: any, topic: string, initialState: Object) {
-  if (!channel) {
-    channel = socket.channel(topic, { token: "123" });
-  }
   if (!stateProxy) {
     stateProxy = proxy(initialState);
   }
-  if (!channel.joinedOnce) {
-    console.log(channel);
-    channel
-      .join()
-      .receive("ok", ({ messages }) => console.log("catching up", messages))
-      .receive("error", ({ reason }) => console.log("failed join", reason))
-      .receive("timeout", () =>
-        console.log("Networking issue. Still waiting...")
-      );
+  if (!channel) {
+    channel = socket.channel(topic, { token: "123" });
+    channel.join();
     channel.on("state:change", (payload: { state: Object }) => {
-      console.log("payload: ", payload);
       Object.assign(stateProxy, payload.state);
     });
-    console.log(channel);
   }
 
   const pushFn = (evt: String, payload: Object) =>
