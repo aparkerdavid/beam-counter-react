c1c4cd3e0b74e052fa3e63a1e6e5dc4805efa68e
Parker Rueve <parkerrueve@gmail.com>
2022-10-09 21:13:26 -0400

    don't rejoin the channel everytime (but make it gross)

--- src/live-state.ts
+++ src/live-state.ts
@@ -1,14 +1,18 @@
 import { proxy, useSnapshot } from "valtio";
 import { useEffect } from "react";
 
-let stateProxy = null;
+let stateProxy;
+let channel;
+
 export function useLiveState(socket: any, topic: string, initialState: Object) {
-  const channel = socket.channel(topic, { token: "123" });
+  if (!channel) {
+    channel = socket.channel(topic, { token: "123" });
+  }
   if (!stateProxy) {
     stateProxy = proxy(initialState);
   }
-  console.log(channel);
   if (!channel.joinedOnce) {
+    console.log(channel);
     channel
       .join()
       .receive("ok", ({ messages }) => console.log("catching up", messages))
@@ -20,8 +24,8 @@ export function useLiveState(socket: any, topic: string, initialState: Object) {
       console.log("payload: ", payload);
       Object.assign(stateProxy, payload.state);
     });
+    console.log(channel);
   }
-  console.log(channel);
 
   const pushFn = (evt: String, payload: Object) =>
     channel.push(`lvs_evt:${evt}`, payload);
