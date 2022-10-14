32a309fa1bcc8f4d8ab7c69602ae63a71d5de6f9
Parker Rueve <parkerrueve@gmail.com>
2022-10-14 13:01:42 -0400

    decruft

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Socket } from "phoenix";
 import { useLiveState } from "./live-state";
 
-let socket = new Socket("ws://localhost:4000/socket", {
+let socket = new Socket("wss://beam-counter.fly.dev/socket", {
   params: { userToken: "123" },
 });
 socket.connect();
@@ -12,26 +11,11 @@ function App() {
   const [pushEvent, state] = useLiveState(socket, "counter", { count: 0 });
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => pushEvent("increment", {})}>
-          count is {state.count}
+          {state.count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 }
